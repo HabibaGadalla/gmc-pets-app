dca82310163e49cf8ec735423f95e078cb97d67f
HabibaGadalla <habiba.m.g@gmail.com>
2021-05-07 12:05:59 +0200

    chore: remove console log

--- src/hooks/filterHook.js
+++ src/hooks/filterHook.js
@@ -4,8 +4,6 @@ const useFilter = (filterName, filterValue, petsList) => {
   const filteredList = petsList.filter(
     (pet) => pet[filterName] === filterValue
   );
-  console.log("filteredList", filteredList);
-
   return filteredList;
 };
 
